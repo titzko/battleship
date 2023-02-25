6f7436dc9db3b66f9d4ba3cd45854dd37f3447fa
titzko <matizko@gmail.com>
2023-02-25 13:32:57 +0100

    sets up jest

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-import _ from "lodash";
 import "./style.scss";
 import { shipFactory } from "./factories/shipFactory";
 
 function component() {
-	const element = document.createElement("div");
-	const btn = document.createElement("button");
-
-	element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-	btn.innerHTML = "Click me and check the console!";
-	btn.onclick = () => alert("hi");
-
-	element.appendChild(btn);
-
+	const el = document.createElement("div");
+	el.innerHTML = "hello Worlddd";
 	let ship1 = shipFactory("my first battleship");
 	ship1.printParam();
 
-	return element;
+	return el;
 }
 
 document.body.appendChild(component());
