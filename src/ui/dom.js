5fd681a1a8afd6fc5ddabbbaf131491140fd45ac
titzko <matizko@gmail.com>
2023-03-17 23:20:23 +0100

    add dragbale event for placing ships

--- src/ui/dom.js
+++ src/ui/dom.js
@@ -20,13 +20,32 @@ const dom = () => {
 		const pageWrapper = createElement("div", "headline", body, "container");
 		const playerBoard = renderBoard("player-board", "Enemy Board", "player");
 		const computerBoard = renderBoard("computer-board", "Your Board", "computer");
+		computerBoard.addEventListener("dragover", function (event) {
+			event.preventDefault();
+		});
+		computerBoard.addEventListener("drop", function (event) {
+			event.preventDefault();
+			//console.log(event.dataTransfer.getData("text/plain"));
+
+			//console.log(event.target); <- this is a cell
+			const data = JSON.parse(event.dataTransfer.getData("text/plain"));
+			const target = event.target;
+
+			let valid = false;
+			data.direction === "x"
+				? (valid = data.amount + Number(target.dataset.x) <= GAME_DIMENSION)
+				: (valid = data.amount + Number(target.dataset.y) <= GAME_DIMENSION);
+
+			valid && event.target.click();
+		});
+
 		pageWrapper.appendChild(playerBoard);
 		pageWrapper.appendChild(computerBoard);
 
 		this.footer = createElement("div", "footer");
 	};
 
-	function renderBoard(id, labelText, user) {
+	const renderBoard = (id, labelText, user) => {
 		const wrapper = document.createElement("div");
 		wrapper.classList.add("board-wrapper");
 
@@ -36,9 +55,9 @@ const dom = () => {
 		const label = createElement("div", "", wrapper, "board-label");
 		label.innerHTML = labelText;
 		return wrapper;
-	}
+	};
 
-	function renderCells(board, user) {
+	const renderCells = (board, user) => {
 		for (let i = 0; i < GAME_DIMENSION; i++) {
 			let row = createElement("div", "", board, "row");
 			for (let j = 0; j < GAME_DIMENSION; j++) {
@@ -48,7 +67,7 @@ const dom = () => {
 				cell.dataset.x = j;
 			}
 		}
-	}
+	};
 
 	function initShipPlacements(ships) {
 		this.shipsToPlace = [...ships];
@@ -61,13 +80,30 @@ const dom = () => {
 		for (let i = 0; i < ship; i++) {
 			createElement("div", "", wrapper, "cell");
 		}
+
+		wrapper.setAttribute("draggable", "true");
+		wrapper.dataset.length = ship;
+		wrapper.dataset.direction = "x";
+
+		wrapper.addEventListener("dragstart", function (event) {
+			event.target.classList.add("dragging");
+			event.dataTransfer.setData("text/plain", JSON.stringify({ direction: "x", amount: ship }));
+		});
+		wrapper.addEventListener("dragend", function (event) {
+			// Remove the 'dragging' class from the element
+			event.target.classList.remove("dragging");
+		});
+	}
+
+	function updateLabel() {
+		this.headline.innerHTML = "Attack the enemy now!";
 	}
 
 	function removePlacementShip() {
 		this.footer.firstElementChild.remove();
 	}
 
-	return { initPage, initShipPlacements, headline, footer, shipsToPlace, removePlacementShip };
+	return { initPage, initShipPlacements, headline, footer, shipsToPlace, removePlacementShip, updateLabel };
 };
 
 export { dom };
