234b8284133d1a79a73190efcf72c2e6568d1c0d
titzko <matizko@gmail.com>
2023-03-18 13:38:46 +0100

    no click event anymore for ship placements. moved that logic also partialy in the game object.

--- src/factories/game.js
+++ src/factories/game.js
@@ -35,7 +35,6 @@ let gameFactory = () => {
 		this.computer.placeShipsRandomlyOnBoard(board1);
 		// alternative of using call would be to export the function and then -> this.startShipPlacementPhase()
 		startShipPlacementPhase.call(this);
-		console.log(this._player);
 	};
 
 	const createUser = function (name, board, isComputer = false, displayName = "Computer") {
@@ -65,7 +64,7 @@ let gameFactory = () => {
 		}
 	};
 
-	const placeShipOnClick = function (cell, ships) {
+	const placeShipOnDrop = function (cell, ships) {
 		if (ships.length === 0) {
 			return;
 		}
@@ -86,11 +85,11 @@ let gameFactory = () => {
 	const startShipPlacementPhase = function () {
 		const ships = SHIP_DIMENSIONS();
 		this._dom.initShipPlacements(ships);
-		const computerCells = document.querySelectorAll('[data-user="computer"]');
-		computerCells.forEach((cell) => {
-			cell.addEventListener("click", () => {
-				placeShipOnClick.call(this, cell, ships);
-			});
+
+		const self = this;
+		document.getElementById("computer-board").addEventListener("drop", function (e) {
+			const cell = handleDropEvent(e);
+			placeShipOnDrop.call(self, cell, ships);
 		});
 	};
 
@@ -112,6 +111,7 @@ let gameFactory = () => {
 	const startGame = function () {
 		this._dom.updateLabel(`Attack the enemy now ${this._player.displayName}!`);
 		this._dom.removeDirectionButton();
+		// TODO -> this should be a dom method
 		changeBoardState();
 		const cells = [...document.querySelectorAll(`[data-user*="player"]`)];
 		const self = this;
@@ -120,6 +120,31 @@ let gameFactory = () => {
 				handleCellClick.call(self, event);
 			});
 		});
+	};
+
+	const handleDropEvent = (event) => {
+		event.preventDefault();
+		const data = JSON.parse(event.dataTransfer.getData("text/plain"));
+		const target = event.target;
+		target.dataset.direction = data.direction;
+
+		const adjustment = data.cellPos;
+		const y = target.dataset.y - adjustment;
+		const x = target.dataset.x;
+
+		let valid = false;
+		data.direction === "x"
+			? (valid = data.amount + Number(target.dataset.x) <= GAME_DIMENSION)
+			: (valid = data.amount + Number(target.dataset.y - adjustment) <= GAME_DIMENSION);
+
+		if (!valid) {
+			document.getElementById("computer-board").addEventListener("click", stopClickPropagation, true);
+			return;
+		}
+
+		const adjustedCell = document.querySelector(`.cell[data-user="computer"][data-y="${y}"][data-x="${x}"]`);
+
+		return data.direction === "x" ? event.target : adjustedCell;
 	};
 
 	return { _player, computer, init };
